db541ffce126c786e5b19b9f20e012c15fac222f
Ryan Breen <rbreen@vistaprint.com>
2014-04-21 21:05:44 -0400

    Ignore realm in digest string calculations and fix python client.

--- test/client.py
+++ test/client.py
@@ -1,7 +1,10 @@
 import requests
 from oauth_hook import OAuthHook
 
+request = requests.Request('GET', 'http://localhost:8000/job')
 oauth_hook = OAuthHook('','', 'super-insecure-test-key', 'super-insecure-secret', True)
-client = requests.session(hooks={'pre_request': oauth_hook})
-response = client.get('http://localhost:8000/job')
-results = json.loads(response.content)
+request = oauth_hook(request)
+prepared = request.prepare()
+session = requests.session()
+response = session.send(prepared)
+print response.content

76887def724875a0e1eeb9fd7184aea9fd730a73
Ryan Breen <rbreen@vistaprint.com>
2014-05-25 12:17:31 -0400

    Add some more metadata to logstash logs and fix a few bugs creating proxies.

--- auspice.js
+++ auspice.js
@@ -14,19 +14,26 @@ var proxy_manager = require('./lib/proxy_manager.js');
 
 var logger = require('./utils/logger.js').getLogger('auspice');
 
-// We use string trimming throughout the application, so update the prototype here.
-if (typeof(String.prototype.trim) === "undefined") {
-  // Add a strip command to make input parsing cleaner.
-  String.prototype.trim = function() {
-    return String(this).replace(/^\s+|\s+$/g, '');
-  };
+if (!process.env.AUSPICE_CONFIG) {
+  logger.error("No config file defined for Auspice.");
+  process.exit(1);
+}
+
+if (!process.env.AUSPICE_SERVICE_NAME) {
+  logger.error("No service name defined for Auspice.");
+  process.exit(2);
+}
+
+if (!process.env.AUSPICE_VERSION) {
+  logger.error("No version defined for Auspice.");
+  process.exit(3);
 }
 
 try {
-  var config = require('./utils/config_loader.js').getState('config.json');
+  var config = require('./utils/config_loader.js').getState(process.env.AUSPICE_CONFIG);
 } catch(e) {
   logger.error('Failed to load auspice config due to %s', e);
-  process.exit(1);
+  process.exit(4);
 }
 
 process.on('uncaughtException', function(err) {
@@ -34,6 +41,14 @@ process.on('uncaughtException', function(err) {
   // logstash and address the root cause.
   logger.error(err);
 });
+
+// We use string trimming throughout the application, so update the prototype here.
+if (typeof(String.prototype.trim) === "undefined") {
+  // Add a strip command to make input parsing cleaner.
+  String.prototype.trim = function() {
+    return String(this).replace(/^\s+|\s+$/g, '');
+  };
+}
 
 // Create a proxy manager at our configured root dir.  All the real work happens
 // in there.

7aa991e5026f57af38eadc015ddce961c324912f
Brian Weber <bweber@vistaprint.com>
2014-10-24 11:46:53 -0400

    Adding RestSharp sample client

---
+++ test/clients/dotnet/Auspice/RestSharpClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace RestSharpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var consumerKey = "dotnet-test-key";
+            var consumerSecret = File.ReadAllText("..\\..\\keys\\8008\\8080\\" + consumerKey);
+
+            RestClient client = new RestClient("http://localhost:8008/")
+                                    {
+                                        Authenticator =
+                                            OAuth1Authenticator
+                                            .ForProtectedResource(
+                                                consumerKey,
+                                                consumerSecret,
+                                                string.Empty,
+                                                string.Empty)
+                                    };
+
+            var restRequest = new RestRequest("job", Method.POST);
+            restRequest.AddParameter("query", "parameters", ParameterType.QueryString);
+            restRequest.AddParameter("also", "good", ParameterType.QueryString);
+            restRequest.AddParameter("are", "post", ParameterType.GetOrPost);
+            restRequest.AddParameter("parameters", "handled", ParameterType.GetOrPost);
+
+            var response = client.Execute(restRequest);
+            Console.Out.WriteLine(response.Content);
+        }
+    }
+}

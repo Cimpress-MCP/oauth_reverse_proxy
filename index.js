d756baad0eabfbef186f60323348464bec1abf6b
Ryan Breen <rbreen@vistaprint.com>
2014-12-18 11:02:15 -0500

    Clean up initialization.

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-/**
- * If we're running as a Windows service, we'll have an environment variable
- * pointing to the correct working dir for the app.  Update the working dir
- * before doing anything involving relative paths.
- */
-if (process.env.OAUTH_REVERSE_PROXY_HOME) {
-  console.log("Changing home directory to " + process.env.OAUTH_REVERSE_PROXY_HOME);
-  process.chdir(process.env.OAUTH_REVERSE_PROXY_HOME);
-}
-
 var oauth_reverse_proxy = require('./lib');
 
 /**
  * The config path can be provided as an environemnt variable.  If not provided,
  * we choose sane defaults for Windows and non-Windows.
  */
-var config_path = process.env.OAUTH_REVERSE_PROXY_CONFIG_PATH;
-if (!config_path) {
+var config_dir = process.env.OAUTH_REVERSE_PROXY_CONFIG_DIR;
+if (!config_dir) {
   var os = require('os').type().toLowerCase();
   if (os.indexOf('windows') !== -1 || os.indexOf('cygwin') !== -1) {
-    config_path = "C:\\ProgramData\\oauth_reverse_proxy\\config.d\\";
+    config_dir = "C:\\ProgramData\\oauth_reverse_proxy\\config.d\\";
   } else {
-    config_path = "/etc/oauth_reverse_proxy.d/";
+    config_dir = "/etc/oauth_reverse_proxy.d/";
   }
 }
 
@@ -46,7 +36,7 @@ try {
 }
 
 // Create an oauth_reverse_proxy instance at our configured root dir.
-oauth_reverse_proxy.init(config_path, function(err, proxy) {
+oauth_reverse_proxy.init(config_dir, function(err, proxy) {
   // If we caught a fatal error creating the proxies, log it and pause briefly before exiting
   // to give Bunyan a chance to flush this error message.
   if (err) {

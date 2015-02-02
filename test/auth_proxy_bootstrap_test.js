8ec2abae4f6c458c3b4900127a0bad5b16f46379
Ryan Breen <rbreen@vistaprint.com>
2015-02-02 17:59:44 -0500

    Begin big dig of making proxies stoppable and startable.

--- test/auth_proxy_bootstrap_test.js
+++ test/auth_proxy_bootstrap_test.js
@@ -79,8 +79,8 @@ describe('oauth_reverse_proxy bootstrap', function() {
   it ('should start cleanly', function(done) {
     oauth_reverse_proxy.init('./test/config.d', function(err, proxies) {
       if (err) done('oauth_reverse_proxy startup failed: ' + err);
-      exports.proxies = proxies;
-      exports.proxy = proxies["jobs_service.json"];
+      exports.proxies = require('../lib/proxy_manager.js').proxies;
+      exports.proxy = exports.proxies["jobs_service.json"];
 
       if (typeof exports.proxy === 'string') {
         should.fail(exports.proxy);

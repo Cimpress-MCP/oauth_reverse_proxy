2012aa7210531eaead08d972f85a060a2f880542
Ryan Breen <rbreen@vistaprint.com>
2015-02-02 23:13:47 -0500

    Add a test sending an authenticated request to a dynamically created proxy.

--- test/auth_proxy_dynamic_config_test.js
+++ test/auth_proxy_dynamic_config_test.js
@@ -2,28 +2,47 @@ var fs = require('fs');
 var should = require('should');
 var _ = require('underscore');
 
-var keygen = require('../utils/keygen.js');
 var auth_proxy_bootstrap_test = require('./auth_proxy_bootstrap_test.js');
+var request_sender = require('./utils/request_sender.js');
+
+var copy_file = function(source, target, cb) {
+  var rd = fs.createReadStream(source);
+  rd.on("error", function(err) {
+    done(err);
+  });
+  var wr = fs.createWriteStream(target);
+  wr.on("error", function(err) {
+    done(err);
+  });
+  wr.on("close", function(ex) {
+    done();
+  });
+  rd.pipe(wr);
+
+  var cbCalled = false;
+  function done(err) {
+    if (!cbCalled) {
+      cb(err);
+      cbCalled = true;
+    }
+  }
+};
 
 describe('oauth_reverse_proxy key loader', function() {
 
-  it ('should support adding keys dynamically', function(done) {
-    keygen.createKey('./test/keys', 8008, 8080, 'dynamic-key', function(err) {
-      keygen.createKey('./test/keys', 8008, 8080, 'dynamic-key2', function(err) {
-        if (err) done(err);
-        var check_key = function() {
-          if (auth_proxy_bootstrap_test.proxy.keystore.keys['dynamic-key']) {
-            // Turn the proxy.keys object into an array to get its length
-            auth_proxy_bootstrap_test.proxy.keystore.count.should.be.exactly(16);
-            done();
-          } else setTimeout(check_key, 50);
-        };
-
-        check_key();
-      });
+  it ('should support adding proxies dynamically', function(done) {
+    copy_file('./test/resources/dynamic_config_service.orig.json', './test/config.d/dynamic_config_service.json', function(err) {
+      if (err) return done(err);
+      var check_config = function() {
+        if (auth_proxy_bootstrap_test.proxies['dynamic_config_service.json']) {
+          request_sender.sendAuthenticatedRequest('GET', 'http://localhost:8010/job/12345', null, 200, done);
+        } else setTimeout(check_config, 50);
+      };
+      check_config();
     });
   });
 
+/**
   it ('should support updating keys dynamically', function(done) {
     keygen.createKey('./test/keys', 8008, 8080, 'dynamic-key', 'happy-fun-key', function(err) {
       if (err) done(err);
@@ -56,4 +75,5 @@ describe('oauth_reverse_proxy key loader', function() {
       });
     });
   });
+**/
 });

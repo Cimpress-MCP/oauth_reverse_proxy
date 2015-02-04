037e6d42eb1bd9ae44a707b2382a3cc249895b16
Ryan Breen <rbreen@vistaprint.com>
2015-02-03 20:14:18 -0500

    Add test that a proxy can be reloaded without a change of port.

--- test/auth_proxy_dynamic_config_test.js
+++ test/auth_proxy_dynamic_config_test.js
@@ -33,30 +33,48 @@ describe('oauth_reverse_proxy config loader', function() {
   it ('should support adding proxies dynamically', function(done) {
     copy_file('./test/resources/dynamic_config_service.orig.json', './test/config.d/dynamic_config_service.json', function(err) {
       if (err) return done(err);
-      var check_config = function() {
-        if (auth_proxy_bootstrap_test.proxies['dynamic_config_service.json']) {
-          request_sender.sendAuthenticatedRequest('GET', 'http://localhost:8010/job/12345', null, 200, done);
-        } else setTimeout(check_config, 50);
-      };
-      check_config();
+      copy_file('./test/resources/dynamic_whitelist_config_service.orig.json', './test/config.d/dynamic_whitelist_config_service.json', function(err) {
+        if (err) return done(err);
+        var check_config = function() {
+          if (auth_proxy_bootstrap_test.proxies['dynamic_config_service.json']) {
+            request_sender.sendAuthenticatedRequest('GET', 'http://localhost:8010/job/12345', null, 200, function(err) {
+              if (err) return done(err);
+
+              request_sender.sendRequest('GET', 'http://localhost:8012/superlivecheck', null, 404, done);
+            });
+          } else setTimeout(check_config, 50);
+        };
+        check_config();
+      });
     });
   });
 
   it ('should support updating proxies dynamically', function(done) {
     copy_file('./test/resources/dynamic_config_service.next.json', './test/config.d/dynamic_config_service.json', function(err) {
       if (err) return done(err);
-      var check_config = function() {
-        if (auth_proxy_bootstrap_test.proxies['dynamic_config_service.json'].config.from_port == 8011) {
-          request_sender.sendAuthenticatedRequest('GET', 'http://localhost:8011/job/12345', null, 200, function(err) {
-            if (err) return done(err);
-            request_sender.sendAuthenticatedRequest('GET', 'http://localhost:8010/job/12345', null, 500, function(err) {
-              err.message.should.equal('connect ECONNREFUSED');
-              done();
+      copy_file('./test/resources/dynamic_whitelist_config_service.next.json', './test/config.d/dynamic_whitelist_config_service.json', function(err) {
+        if (err) return done(err);
+        var check_config = function() {
+          // We poll until the config for dynamic_config_service.json has been updated to listen on port 8011 instead of 8010.  This tells us that
+          // the proxy manager has reloaded config.
+          if (auth_proxy_bootstrap_test.proxies['dynamic_config_service.json'].config.from_port == 8011) {
+            request_sender.sendAuthenticatedRequest('GET', 'http://localhost:8011/job/12345', null, 200, function(err) {
+              if (err) return done(err);
+              request_sender.sendAuthenticatedRequest('GET', 'http://localhost:8010/job/12345', null, 500, function(err) {
+                err.message.should.equal('connect ECONNREFUSED');
+
+                // Validate that dynamic_whitelist_config_service has been updated to reflect the whitelist in dynamic_whitelist_config_service.next.json.
+                // /superlivecheck should no longer be whitelisted, but /superduperlivecheck will be.
+                request_sender.sendRequest('GET', 'http://localhost:8012/superlivecheck', null, 400, function(err) {
+                  if (err) return done(err);
+                  request_sender.sendRequest('GET', 'http://localhost:8012/superduperlivecheck', null, 404, done);
+                });
+              });
             });
-          });
-        } else setTimeout(check_config, 50);
-      };
-      check_config();
+          } else setTimeout(check_config, 50);
+        };
+        check_config();
+      });
     });
   });
 

e05c6ec7fd05143e32306b6fb9bd0f0d2a98ca70
Ryan Breen <rbreen@cimpress.com>
2015-06-24 16:13:24 -0400

    Add new test case.

---
+++ test/message_integrity_encoding_test.js
@@ -0,0 +1,51 @@
+// All the messy business of creating and sending requests (both authenticated and unauthenticated)
+// lives in request_sender.
+var request_sender = require('./utils/request_sender.js');
+var validation_tools = require('./utils/validation_tools.js');
+
+// All tests must require bootstrap_test since that creates our proxy, starts our job server,
+// and registers a beforeEach to keep the request_sender and job_server clean between test runs.
+require('./bootstrap_test.js');
+
+// Run these tests in two modes, one where the outbound request is signed by the proxy and the other
+// where a signed request is sent to a reverse proxy.
+['oauth_proxy', 'oauth_reverse_proxy'].forEach(function(mode) {
+
+  // Tests that URL encoding is handled correctly by oauth_reverse_proxy
+  describe(mode + ' message integrity: URL encoding', function() {
+
+    var sendFn = mode === 'oauth_reverse_proxy' ?
+      request_sender.sendAuthenticatedRequest :
+      request_sender.sendProxyAuthenticatedRequest;
+
+    var simpleSendFn = mode === 'oauth_reverse_proxy' ?
+      request_sender.sendSimpleAuthenticatedRequest :
+      request_sender.sendSimpleProxyAuthenticatedRequest;
+
+    // We want to test that giant query strings aren't allowed for any verb, so loop over them all
+    ['GET', 'DELETE'].forEach(function(verb) {
+      // Validate that a GET or DELETE with query parameters works.
+      it ("should accept a properly signed " + verb + " with a funky query", function(done) {
+        request_sender.params.push(['qamper&and', "%%%%froody%%%%"]);
+        request_sender.params.push(['query', 'funky town']);
+        sendFn(verb, 'http://localhost:8008/job/12345?query=funky%20town&qamper%26and=%25%25%25%25froody%25%25%25%25', null, 200, done);
+      });
+
+      // Validate that a GET or DELETE with an empty query value works.
+      it ("should accept a properly signed " + verb + " with param that has no value", function(done) {
+        request_sender.params.push(['qamper&and', '']);
+        request_sender.params.push(['query', 'funky town']);
+        sendFn(verb, 'http://localhost:8008/job/12345?qamper%26and=&query=funky%20town', null, 200, done);
+      });
+    });
+
+    // We want to test that giant query strings aren't allowed for any verb, so loop over them all
+    ['GET', 'POST', 'PUT', 'DELETE'].forEach(function(verb) {
+      // Validate that a GET or DELETE with query parameters works.
+      it ("should accept a properly signed " + verb + " with a funky path", function(done) {
+        //sendFn(verb, 'http://localhost:8008/{wonky path}/is&wonky', null, 200, done);
+        sendFn(verb, 'http://localhost:8008/%7bwonky%20path%7d/is&wonky', null, 200, done);
+      });
+    });
+  });
+});

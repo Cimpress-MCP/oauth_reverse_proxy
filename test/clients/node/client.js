31bd1efb950b7b5859ac3a10098079bd59d5b3d8
Theodore X. Pak <tpak@cimpress.com>
2015-10-05 11:38:18 -0400

    Code formatting

--- test/clients/node/client.js
+++ test/clients/node/client.js
@@ -1,19 +1,19 @@
-var util = require('util');
+var util = require("util");
 
-var consumer_key = 'node-test-key';
-var consumer_secret = require('fs').readFileSync('../../keys/8008/8080/' + consumer_key);
+var consumer_key = "node-test-key";
+var consumer_secret = require("fs").readFileSync("../../keys/8008/8080/" + consumer_key);
 
-var oauth = require('oauth').OAuth;
-var oa = new oauth('', '', consumer_key, consumer_secret, '1.0', null, 'HMAC-SHA1');
+var oauth = require("oauth").OAuth;
+var oa = new oauth("", "", consumer_key, consumer_secret, "1.0", null, "HMAC-SHA1");
 
 var post_body = {
-  posts:'should',
-  fun: 'kill',
-  beans: 'either'
+  posts: "should",
+  fun: "kill",
+  beans: "either"
 };
 
 oa.post("http://localhost:8008/job?do=query&strings=kill&us=or&not=true", "", "",
-  post_body, 'application/x-www-form-urlencoded',
+  post_body, "application/x-www-form-urlencoded",
   function (error, data, response) {
     if (error) {
       console.error("Got error %s", util.inspect(error));
